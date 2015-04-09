41ba25ab42fc6b1559f472bbd9b00ece62b41949
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-04-09 13:22:24 +0200

    Added measure utility and naive.cpp to show how to empirically measure algorithms

--- Makefile
+++ Makefile
@@ -10,8 +10,11 @@ DOC = ./doc
 
 all: bin/genera-duplicados
 
-bin/genera-duplicados: src/genera-duplicados.cpp
-	$(CC) $(CPPFLAGS) src/genera-duplicados.cpp -o bin/genera-duplicados
+bin/genera-duplicados: src/util/genera-duplicados.cpp
+	$(CC) $(CPPFLAGS) src/util/genera-duplicados.cpp -o bin/genera-duplicados
+
+bin/naive: src/naive.cpp src/util/measure.h
+	$(CC) $(CPPFLAGS) src/naive.cpp -o bin/naive
 
 doc:
 	doxygen $(DOC)/doxys/Doxyfile
---
+++ src/util/measure.h
@@ -0,0 +1,39 @@
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <climits>
+
+
+using namespace std;
+using namespace std::chrono;
+
+namespace measure {
+	
+	vector<int> getInput(int input_size) {
+		srand(time(0));
+		vector<int> result;
+
+		for(int i = 0; i < input_size; i++){
+ 			result.push_back(rand() % (input_size - 1));	// Make sure there is at least one duplicate
+		}
+
+		return result;
+	}
+
+
+	typedef vector<int> (*RemoveDuplicatesFunction)(vector<int>);
+
+	int measure(RemoveDuplicatesFunction function, int input_size){
+
+		vector<int> input = getInput(input_size);
+
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		function(input);
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+
+		return duration;
+	}
+
+}
